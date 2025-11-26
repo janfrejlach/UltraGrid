22902bf0a6e6f894ce3c9a14c87927aa9903e6f9
Jan Frejlach <jan.frejlach@gmail.com>
2025-11-26 09:49:34 +0100

    jpegxs measurement

---
+++ src/jpegxs/jpegxs_measurement.h
@@ -0,0 +1,53 @@
+#include <memory>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+#include <float.h>
+
+#include "debug.h"
+
+
+struct measurement {
+        const char *name;
+
+        double total_time = 0.0;
+        double min_ms = DBL_MAX;
+        double max_ms = 0.0;
+
+        uint64_t skip = 60;
+        uint64_t count = 0;
+
+        void update(std::chrono::steady_clock::time_point start_time, std::chrono::steady_clock::time_point end_time);
+        void print();
+};
+
+void measurement::update(std::chrono::steady_clock::time_point start_time, std::chrono::steady_clock::time_point end_time) {
+        if (count < skip) {
+                count++;
+                return;
+        }
+        
+        auto diff = end_time - start_time;
+        double duration = std::chrono::duration<double, std::milli>(diff).count();
+
+        count++;
+        total_time += duration;
+        if (duration < min_ms) min_ms = duration;
+        if (duration > max_ms) max_ms = duration;
+}
+
+void measurement::print() {
+        if (count < skip) {
+                log_msg(LOG_LEVEL_INFO, "%s: not enough samples\n", name);
+                return;
+        }
+        
+        double avg = total_time / (count - skip);
+
+        log_msg(LOG_LEVEL_INFO, "%s: avg=%.3f ms  min=%.3f ms  max=%.3f ms\n", name, avg, min_ms, max_ms);
+}
+
+struct timestamp {
+    std::chrono::steady_clock::time_point t;
+};
--- src/video_compress/jpegxs.cpp
+++ src/video_compress/jpegxs.cpp
@@ -17,6 +17,7 @@
 #include "utils/video_frame_pool.h"
 #include "utils/synchronized_queue.h"
 #include "jpegxs/jpegxs_conv.h"
+#include "jpegxs/jpegxs_measurement.h"
 
 #define DEFAULT_POOL_SIZE 5
 #define MOD_NAME "[JPEG XS enc.] "
@@ -33,50 +34,6 @@ using std::atomic;
 
 namespace {
 struct state_video_compress_jpegxs;
-
-struct measurement {
-        const char *name;
-
-        double total_time = 0.0;
-        double min_ms = DBL_MAX;
-        double max_ms = 0.0;
-
-        uint64_t skip = 60;
-        uint64_t count = 0;
-
-        void update(std::chrono::steady_clock::time_point start_time, std::chrono::steady_clock::time_point end_time);
-        void print();
-};
-
-void measurement::update(std::chrono::steady_clock::time_point start_time, std::chrono::steady_clock::time_point end_time) {
-        if (count < skip) {
-                count++;
-                return;
-        }
-        
-        auto diff = end_time - start_time;
-        double duration = std::chrono::duration<double, std::milli>(diff).count();
-
-        count++;
-        total_time += duration;
-        if (duration < min_ms) min_ms = duration;
-        if (duration > max_ms) max_ms = duration;
-}
-
-void measurement::print() {
-        if (count < skip) {
-                log_msg(LOG_LEVEL_INFO, "%s: not enough samples\n", name);
-                return;
-        }
-        
-        double avg = total_time / (count - skip);
-
-        log_msg(LOG_LEVEL_INFO, "%s: avg=%.3f ms  min=%.3f ms  max=%.3f ms\n", name, avg, min_ms, max_ms);
-}
-
-struct timestamp {
-    std::chrono::steady_clock::time_point t;
-};
 
 struct state_video_compress_jpegxs {
         state_video_compress_jpegxs(struct module *parent, const char *opts);
