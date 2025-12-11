2a1d8a9af220180f502470744e998119a307b722
Jan Frejlach <jan.frejlach@gmail.com>
2025-12-11 11:37:35 +0100

    jpegxs measurement: standard deviation

--- src/jpegxs/jpegxs_measurement.h
+++ src/jpegxs/jpegxs_measurement.h
@@ -1,4 +1,6 @@
 #include <float.h>
+#include <vector>
+#include <cmath>
 
 #include "debug.h"
 
@@ -9,7 +11,8 @@ struct timestamp {
 struct measurement {
         const char *name;
 
-        double total_time = 0.0;
+        std::vector<double> values{};
+
         double min_ms = DBL_MAX;
         double max_ms = 0.0;
 
@@ -30,7 +33,7 @@ inline void measurement::update(std::chrono::steady_clock::time_point start_time
         double duration = std::chrono::duration<double, std::milli>(diff).count();
 
         count++;
-        total_time += duration;
+        values.push_back(duration);
         if (duration < min_ms) min_ms = duration;
         if (duration > max_ms) max_ms = duration;
 }
@@ -40,8 +43,20 @@ inline void measurement::print() {
                 log_msg(LOG_LEVEL_INFO, "%s: not enough samples\n", name);
                 return;
         }
-        
-        double avg = total_time / (count - skip);
 
-        log_msg(LOG_LEVEL_INFO, "%s: avg=%.3f ms  min=%.3f ms  max=%.3f ms\n", name, avg, min_ms, max_ms);
+        const size_t n = values.size();
+
+        double sum = 0.0;
+        for (double v : values) sum += v;
+        double avg = sum / n;
+
+        double variance_sum = 0.0;
+        for (double v : values) {
+                double diff = v - avg;
+                variance_sum += diff * diff;
+        }
+
+        double stddev = (n > 1) ? std::sqrt(variance_sum / (n - 1)) : 0.0;
+
+        log_msg(LOG_LEVEL_INFO, "%s: avg=%.3f ms  min=%.3f ms  max=%.3f ms  stddev=%.3f ms\n", name, avg, min_ms, max_ms, stddev);
 }
